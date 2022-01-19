034086709e4957c6ea60df9ccaa0cc7dd99a9a4b
Andika Eka Kurnia <73420758+DikDns@users.noreply.github.com>
2022-01-19 11:31:17 +0700

    Class & Object 🟢

---
+++ 00-class-object/index.php
@@ -0,0 +1,19 @@
+<?php
+
+    // CLASS
+    class Players {
+
+    }
+
+    // OBJECT
+    $dikdns = new Players();
+    $doobwuz = new Players();
+    $aidenzyy = new Players();
+
+    // SHOW OBJECT
+    var_dump( $dikdns);
+    var_dump( $doobwuz);
+    var_dump( $aidenzyy);
+
+
+?>
